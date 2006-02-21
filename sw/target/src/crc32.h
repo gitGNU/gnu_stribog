f9ffa4be66a25d3cca3b4a8e7be9cf8f1cc974fb
P.Kharitonov <ineiev@users.sourceforge.net>
2006-02-21 10:11:27 +0000

    the algorithm itself moved to crc32.c

--- sw/target/src/crc32.h
+++ sw/target/src/crc32.h
@@ -1,7 +1,5 @@
-/*CRC32(CCITT-32): adjusted to 32-bit words algorithm from
-Robert L. Hummel 
-Programmer's Technical Reference: Data and Fax Communication
-TODO: check
+/*CRC32(CCITT-32) checksum
+NB current implementation works on 32-bit little-endian architectures only
 This file has been written for the stribog project.
 
 This program is free software; you can redistribute it and/or modify
@@ -19,12 +17,4 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
 
 Copyright (C) 2006 D.Ineiev <ineiev@yahoo.co.uk>*/
-#define CRC_POLY (0xEDB88320)
-/*NB these will work on 32-bit architectures only*/
-static unsigned form_crc(unsigned*msg,unsigned n)
-{unsigned crc=~0;
- while(n-->0)
- {int j,k;crc^=*msg++;
-  for(j=0;j<32;j++){k=crc&1;crc>>=1;if(k)crc^=CRC_POLY;}
- }return~crc;
-}
+unsigned form_crc(unsigned*msg,unsigned n);
