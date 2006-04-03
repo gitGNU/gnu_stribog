ef5cba242f41d850765ac8e311b47162283e032d
P.Kharitonov <ineiev@users.sourceforge.net>
2006-04-03 11:22:32 +0000

    version update; notes on Win32 version

--- sw/host/conloq/usage.c
+++ sw/host/conloq/usage.c
@@ -19,7 +19,7 @@ Copyright (C) 2006 D.Ineiev <ineiev@yahoo.co.uk>*/
 #include"usage.h"
 #include<stdio.h>
 void usage(void)
-{printf("conloq 0?1 ("__DATE__"): talk to stribog\n\n"
+{printf("conloq 0?6 ("__DATE__"): talk to stribog\n\n"
 "This program is a part of the stribog host software section\n\n"
 "This program is free software; you can redistribute it and/or modify\n"
 "it under the terms of the GNU General Public License as published by\n"
@@ -33,5 +33,7 @@ void usage(void)
 "along with this program; if not, write to the Free Software\n"
 "Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA\n\n"
 "Copyright (C) 2006 D.Ineiev <ineiev@yahoo.co.uk>\n"
+"usage: conloq [port]\n default port on GNU/Linux is /dev/ttyS1, on Win32 -- COM1\n\n"
+"all data received are written in conloq.log\n"
   );
 }
