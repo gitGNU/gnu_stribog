32bc8671f7b1888a728320d34c0b89759dba7929
P.Kharitonov <ineiev@users.sourceforge.net>
2006-04-03 11:21:11 +0000

    minor changes

--- sw/host/conloq/conloq.c
+++ sw/host/conloq/conloq.c
@@ -28,9 +28,11 @@ int main(int argc,char**argv)
 {tsip_buf*tb=new_tsip();int size,i=0,n,j;const unsigned char*_;
  unsigned char s[0x121];FILE*f=fopen(log,"wb");
  if(!f){error("can't open file \""log"\"\n");free_tsip(tb);return 1;}
+ init_exp(argc>2?0:1);
  if(initserialia(argc>1?argv[1]:0))
- {fclose(f);free_tsip(tb);error("can't open serial port\n");return 2;}
+ {fclose(f);free_tsip(tb);close_exp();error("can't open serial port\n");return 2;}
  while(1)if(0<(n=lege(s,sizeof(s))))for(j=0;j<n;putc(s[j++],f))
-  if((_=parse_tsip(tb,s[j],&size)))if(!(i++&0x3F))expone(_,size);
- closeserialia();fclose(f);free_tsip(tb);return 0;
+  if((_=parse_tsip(tb,s[j],&size)))
+  {if(size!=24)expone(_,size);else if(!(i++&0x3F))expone(_,size);}
+ closeserialia();close_exp();fclose(f);free_tsip(tb);return 0;
 }
