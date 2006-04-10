f85089221255f2de4048876c3802609b5d76d7dd
P.Kharitonov <ineiev@users.sourceforge.net>
2006-04-10 04:15:21 +0000

    initial add

---
+++ sw/host/conloq/magex-conloq.c
@@ -0,0 +1,36 @@
+/*magex-conloq: listen to magex
+ from UNIX terminal via RS-232 UART
+This program is a part of the stribog host software section
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+
+Copyright (C) 2006 D.Ineiev <ineiev@yahoo.co.uk>*/
+#include"serialia.h"
+#include"parse_tsip.h"
+#include"exp.h"
+#include"error.h"
+#include<stdio.h>
+#define log	"magex-conloq.log"
+int main(int argc,char**argv)
+{tsip_buf*tb=new_tsip();int size,i=0,n,j;const unsigned char*_;
+ unsigned char s[0x121];FILE*f=fopen(log,"wb");
+ if(!f){error("can't open file \""log"\"\n");free_tsip(tb);return 1;}
+ init_exp(argc>2?0:1);
+ if(initserialia(argc>1?argv[1]:0))
+ {fclose(f);free_tsip(tb);close_exp();error("can't open serial port\n");return 2;}
+ while(1)if(0<(n=lege(s,sizeof(s))))for(j=0;j<n;putc(s[j++],f))
+  if((_=parse_tsip(tb,s[j],&size)))if(!(i++&0x3F))expone(_,size);
+ closeserialia();close_exp();fclose(f);free_tsip(tb);return 0;
+}
---
+++ sw/host/conloq/magexp.c
@@ -0,0 +1,31 @@
+/*decode magexp messages
+This program is a part of the stribog host software section
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+
+Copyright (C) 2006 D.Ineiev <ineiev@yahoo.co.uk>*/
+#include"exp.h"
+#include<stdint.h>
+#include"error.h"
+#include<stdio.h>
+int init_exp(int x){return 0;}void close_exp(void){}
+void expone(const unsigned char*s,int size)
+{int mag[3],missed;if(size!=5){error("wrong size (%i for 5)\n",size);return;}
+ *mag=0;if(s[1]&(1<<3))*mag=-1&~(0xFFF);*mag|=*s|((((int)s[1])&0xF)<<8);
+ mag[1]=0;if(s[2]&(1<<7))mag[1]=-1&~(0xFFF);mag[1]|=((s[1]>>4)&0xF)|(((int)s[2])<<4);
+ mag[2]=0;if(s[4]&(1<<3))mag[2]=-1&~(0xFFF);mag[2]|=s[3]|((((int)s[4])&0xF)<<8);
+ if(s[4]&(1<<7))missed=!0;else missed=0;
+ printf("mag: %i %i %i %i\n",missed,*mag,mag[1],mag[2]);
+}
