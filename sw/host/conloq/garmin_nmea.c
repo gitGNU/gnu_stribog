0d3e283a4fecd73355016a0b03ee9928145c1462
P.Kharitonov <ineiev@users.sourceforge.net>
2006-04-14 04:27:10 +0000

    timestamps are now in seconds rather than MCU ticks

--- sw/host/conloq/garmin_nmea.c
+++ sw/host/conloq/garmin_nmea.c
@@ -18,31 +18,26 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
 Copyright (C) 2006 D.Ineiev <ineiev@yahoo.co.uk>*/
 #include<stdio.h>
 #include"exp_gps.h"
+#include"error.h"
 #include<stdint.h>
 #include<string.h>
-static int expone;
-int init_gps(int exp){expone=exp;gps_buf=new_tsip();return 0;}
-void close_gps(void){free_tsip(gps_buf);}
+int init_gps(int exp){return 0;}void close_gps(void){}
 static int field_cmp(const char*s,const char*c)
 {const char*_=strchr(s,',');if(!_)return!0;return strncmp(s,c,_-s);}
-static int parse_gga(const char*s,unsigned long long t)
-{
- return 0;
-}
-static int parse_pgrmv(const char*s,unsigned long long t)
-{
- return 0;
-}
-static int nmea_switch(const char*s,unsigned long long t)
+static int parse_gga(const char*s,double t)
+{printf("GGA %.7f %s\n",t,s);return 0;}
+static int parse_pgrmv(const char*s,double t)
+{printf("RMV %.7f %s\n",t,s);return 0;}
+static int nmea_switch(const char*s,double t)
 {if(!field_cmp(++s,"GPGGA,"))return parse_gga(s+6,t);
- if(!field_cmp(s,"PGRMV,"))return parse_pgrmv(s+6,t);
+ if(!field_cmp(s,"PGRMV,"))return parse_pgrmv(s+6,t);return 0;
 }
-void exp_gps(unsigned long long time,const unsigned char*s,FILE*gps)
+void exp_gps(double time,const unsigned char*s,FILE*gps)
 {int n=s[11],i;static char nmea[0x100];static int k;
  for(i=0;i<n;i++)
  {if(gps)putc(s[i],gps);nmea[k++]=s[i];
   if(k>=sizeof(nmea)){error("too long message");k=0;continue;}
   if(s[i]=='$')
-  {if(*nmea='$'){nmea[k]=0;nmea_switch(nmea,ticks);}k=1;*nmea='$';}
+  {if(*nmea=='$'){nmea[k-3]=0;nmea_switch(nmea,time);}k=1;*nmea='$';}
  }
 }
