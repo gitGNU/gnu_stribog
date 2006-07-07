bd11da6de5fa1307f6f1d87f73622e621ba54bf7
P.Kharitonov <ineiev@users.sourceforge.net>
2006-07-07 08:19:59 +0000

    some variants of raw acceleration measurements processing shown in comments

--- sw/target/src/multa.c
+++ sw/target/src/multa.c
@@ -42,18 +42,49 @@ static void check_gps_data(void)
  if(!(data_got=receive0(s,sizeof(_)-1)))return;s[sizeof(_)-1]=data_got;
  data_got=send_fix(_,GPS_DATA_LENGTH);
 }
+static void correct_adc(unsigned w[3],unsigned t[3],unsigned _2048)
+{static unsigned _8192,_,n,v,k;int i,d;static const int a0=641;
+ _+=_2048;if(n++==3){v=!0;n=0;_8192=(_>>2)-a0;_=0;}
+ if(v)d=_8192;else d=_2048-a0;k=((a0*a0-d*(a0+20)+d*d)*5+(42<<10));
+ for(i=0;i<3;i++){w[i]=((w[i]*k)>>21);t[i]=((t[i]*k)>>21);}
+}
+/*static inline int median(int*a)//we want a better program to filter spurs
+{if(*a>=a[1])
+ {if(a[1]>=a[2])return a[1];else{if(a[2]<=*a)return a[2];else return*a;}
+ }else
+ {if(*a>=a[2])return*a;else{if(a[2]<=a[1])return a[2];else return a[1];}}
+ return*a;
+}
+static void process_accel(unsigned*out)
+{const unsigned*acc;static int i,a[3][3],j,v,spur[3];if(!(acc=get_accel()))return;
+ if(++i>2){i=0;v=!0;}if(!v){for(j=0;j<3;j++){a[j][i]=out[j]=acc[j];}return;}
+ for(j=0;j<3;j++)
+ {if(spur[j]){a[j][i]=acc[j];spur[j]=0;}
+  else{int d=a[j][i]-(int)acc[j];if(d<-1000||d>1000){spur[j]=d;continue;}else a[j][i]=acc[j];}
+  out[j]=median(a[j]);
+ }
+}*/
+/*static void process_accel(unsigned*out)//this will cut blows. design it at the host side first
+{int j,d;const unsigned*acc;static int i,spur[3];if(!(acc=get_accel()))return;
+ for(j=0;j<3;j++)
+ {d=(int)acc[j]-out[j];
+  if(d<-1000||d>1000){if(spur[j]&0x7==0x7)out[j]=acc[j];spur[j]|=1<<i;}
+  else{out[j]=acc[j];spur[j]=0;}
+ }if(i++>2)i=0;
+}*/
+static void process_accel(unsigned*out)
+{int j;const unsigned*a;if(!(a=get_accel()))return;for(j=0;j<3;j++)out[j]=a[j];}
 static int send_msmt(void)
-{static unsigned f[FIX_LENGTH];const int*mag;unsigned*s=f;int t,w[3];
- const unsigned*adc,*acc;static int i,pending;static int b[3],a[3];
+{static unsigned f[FIX_LENGTH],a[3];const int*mag;unsigned*s=f,w[3];int t;
+ const unsigned*adc;static int i,pending;static int b[3];
  if(pending){pending=send_fix(f,FIX_LENGTH);/*if(!pending){led1_set();led1_clr();}*/}
  if(!(adc=get_adc()))return 0;_2048=adc[ADC_2048];
  adxrs_temp[0]=adc[ADC_TX];adxrs_temp[1]=adc[ADC_TY];adxrs_temp[2]=adc[ADC_TZ];
- if(mag=process_mag(adc)){if(i++&3)return 0;}else return 0;
- *b=*mag;b[1]=mag[1];b[2]=mag[2];
- t=get_temperature();w[0]=adc[ADC_WX];w[1]=adc[ADC_WY];w[2]=adc[ADC_WZ];
- if((acc=get_accel())){a[0]=acc[0];a[1]=acc[1];a[2]=acc[2];}
- *s++=iunius_tempus();
- *s++=(b[0]&0xFFF)|((b[1]&0xFFF)<<12)|((t&0xFF)<<24);
+ if((mag=process_mag(adc))){if(i++&3)return 0;}else return 0;
+ *b=*mag;b[1]=mag[1];b[2]=mag[2];t=get_temperature();
+ w[0]=adc[ADC_WX];w[1]=adc[ADC_WY];w[2]=adc[ADC_WZ];
+ correct_adc(w,adxrs_temp,_2048);process_accel(a);
+ *s++=iunius_tempus();*s++=(b[0]&0xFFF)|((b[1]&0xFFF)<<12)|((t&0xFF)<<24);
  *s++=(b[2]&0xFFF)|((a[0]&0x3FFF)<<12)|((t&0x1F00)<<(26-8));
  *s++=(a[1]&0x3FFF)|((a[2]&0x3FFF)<<14);
  *s=(w[0]&0x3FF)|((w[1]&0x3FF)<<10)|((w[2]&0x3FF)<<20);
@@ -67,7 +98,7 @@ static void send_temp(int ms)
  if(!send_fix(t,TEMP_LENGTH))i=0;
 }
 int main(void)
-{int j=0,ms;start_pll();init_power();init_led();init_lm74();init_accel();led1_clr();
+{int j=0,ms;start_pll();init_power();init_led();init_lm74();init_accel();//led1_clr();
  init_mag();connect_pll();init_adc();init_uart1();init_uart0();init_tempus();
  while(1)
  {ms=send_msmt();send_temp(ms);send_pps();check_gps_data();
