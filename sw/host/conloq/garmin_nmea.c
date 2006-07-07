18578545e287ff6c24e95a07273393fb2a1cfe36
P.Kharitonov <ineiev@users.sourceforge.net>
2006-07-07 08:11:04 +0000

    brief form for printing added

--- sw/host/conloq/garmin_nmea.c
+++ sw/host/conloq/garmin_nmea.c
@@ -22,18 +22,19 @@ Copyright (C) 2006 D.Ineiev <ineiev@yahoo.co.uk>*/
 #include"error.h"
 #include<string.h>
 #include<math.h>
-static double t_pos=-2,lla[3],vel[3];
+static double t_pos=-2,lla[3],vel[3];static int brief;
 static void add_point(void)
 {gps_point g;g.utc=t_pos;g.lat=lla[0];g.lon=lla[1];g.alt=lla[2];
  g.east_vel=vel[0];g.north_vel=vel[1];g.up_vel=vel[2];add_gps_point(&g);
 }
-int init_gps(int exp){return 0;}void close_gps(void){}
+int init_gps(int exp){brief=!exp;return 0;}void close_gps(void){}
 static int field_cmp(const char*s,const char*c)
 {const char*_=strchr(s,',');if(!_)return!0;return strncmp(s,c,_-s);}
 static const char*nmea_field(const char*s,int n)
 {int i;const char*_=s;for(i=0;i<n&&_;i++)_=strchr(_+1,',');if(_&&n)_++;return _;}
 static int parse_gga(const char*s,double t)
 {const char*_;char c;double d,x;int h,m;printf("GGA: %.8f %s\n",t,s);
+ if(brief)return 0;
  if(!(_=nmea_field(s,5)))return!0;if(*_=='0'||*_=='6')return!0;
  if((_=nmea_field(s,0)))
  {sscanf(_,"%lf,",&x);h=x/10000;x-=h*10000;m=x/100;
@@ -51,18 +52,20 @@ static int parse_gga(const char*s,double t)
  return 0;
 }
 static int parse_pgrmv(const char*s,double t)
-{printf("RMV: %.8f %s\n",t,s);sscanf(s,"%lf,%lf,%lf",vel,vel+1,vel+2);
- add_point();return 0;
-}
+{printf("RMV: %.8f %s\n",t,s);if(brief)return 0;
+ sscanf(s,"%lf,%lf,%lf",vel,vel+1,vel+2);add_point();return 0;
+}static int parse_zda(const char*s,double t)
+{printf("ZDA: %.8f %s\n",t,s);return 0;}
 static int nmea_switch(const char*s,double t)
 {if(!field_cmp(++s,"GPGGA,"))return parse_gga(s+6,t);
- if(!field_cmp(s,"PGRMV,"))return parse_pgrmv(s+6,t);return 0;
+ if(!field_cmp(s,"PGRMV,"))return parse_pgrmv(s+6,t);
+ if(!brief)if(!field_cmp(s,"GPZDA,"))return parse_zda(s+6,t);
+ return 0;
 }
 void exp_gps(double time,const unsigned char*s,FILE*gps)
 {int n=s[11],i;static char nmea[0x100];static int k;
  for(i=0;i<n;i++)
- {if(gps)putc(s[i],gps);nmea[k++]=s[i];
-  if(k>=sizeof(nmea)){error("too long message");k=0;continue;}
+ {nmea[k++]=s[i];if(k>=sizeof(nmea)){error("too long message");k=0;continue;}
   if(s[i]=='$')
   {if(*nmea=='$'){nmea[k-3]=0;nmea_switch(nmea,time);}k=1;*nmea='$';}
  }
