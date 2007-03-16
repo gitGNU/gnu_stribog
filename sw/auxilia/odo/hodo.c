6f6cfee3520d6937934355aa8a57da5931f0292e
P.Kharitonov <ineiev@users.sourceforge.net>
2007-03-16 05:05:00 +0000

    initial add

---
+++ sw/auxilia/odo/hodo.c
@@ -0,0 +1,47 @@
+#include<avr/io.h>/*odometer firmware*/
+#define F_CPU   (14745600ul)
+#define BAUD    (9600)
+#define	FRAME	((1<<UCSZ1)|(1<<UCSZ0)|(1<<URSEL))/*8-bit,No parity*/
+#define PERIOD	((unsigned char)(17))
+#define PORT_MASK	(1<<5)
+static unsigned char capta[256],caput,cauda,missa[5],i=sizeof(missa);
+static unsigned cnt;
+static void init(void)
+{UBRRH=0;UBRRL=F_CPU/BAUD/16-1;UCSRB=(1<<RXEN)|(1<<TXEN);UCSRC=FRAME;
+ TCCR0=(1<<CS02)|(1<<CS00);/*clk_io/1024*/
+ TCCR1B=(1<<CS12);/*clk_io/256*/
+}static void cape(void){if(!(UCSRA&(1<<RXC)))return;capta[caput++]=UDR;}
+static void mitte(void)
+{if(!(UCSRA&(1<<UDRE)))return;
+ if(caput!=cauda)UDR=capta[cauda++];else
+ {if(TCNT0<PERIOD&&!(TIFR&(1<<TOV0)))return;TCNT0=0;TIFR|=(1<<TOV0);
+  if(i<sizeof(missa))UDR=missa[i++];
+ }
+}static void pone_mittenda(void)
+{unsigned t=TCNT1;
+ missa[0]=(((unsigned char)cnt)|(3<<6));missa[1]=(cnt>>6)|(3<<6);
+ missa[2]=((unsigned char)t)|(3<<6);
+ missa[3]=((unsigned char)(t>>6))|(3<<6);
+ missa[4]=((unsigned char)(t>>12))|(1<<7);
+ i=0;
+}
+static void inc_cnt(void)
+{static unsigned char c1,c2;unsigned char c=PINC&PORT_MASK;
+ if(!(c1^c)&&(c2^c1))cnt++;c2=c1;c1=c;if(i==sizeof(missa))pone_mittenda();
+}int main(void){init();while(1){inc_cnt();cape();mitte();}return 0;}
+/*This is an auxiliary part of stribog
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+
+Copyright (C) 2007 D.Ineiev <ineiev@yahoo.co.uk>*/
