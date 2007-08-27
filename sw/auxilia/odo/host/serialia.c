c5e795d5b26371452a322ee7b9c27505e4e2be78
P.Kharitonov <ineiev@users.sourceforge.net>
2007-08-27 06:06:22 +0000

    default port changed to /dev/ttyS0

--- sw/auxilia/odo/host/serialia.c
+++ sw/auxilia/odo/host/serialia.c
@@ -1,20 +1,3 @@
-/*conloq: POSIX serial port module
-This program is a part of the stribog host software section
-
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program. If not, see <http://www.gnu.org/licenses/>.
-
-Copyright (C) 2006, 2007 D.Ineiev <ineiev@yahoo.co.uk>*/
 #include"serialia.h"
 #include<unistd.h>
 #include<sys/types.h>
@@ -24,7 +7,7 @@ Copyright (C) 2006, 2007 D.Ineiev <ineiev@yahoo.co.uk>*/
 #include<termios.h>
 #include<stdlib.h>
 #include<stdio.h>
-static struct termios vet,nov;static char dv[]="/dev/ttyS1";
+static struct termios vet,nov;static char dv[]="/dev/ttyS0";
 static int portd=-1;
 int initserialia(const char*tty)
 {portd=open(tty?tty:dv,O_RDWR|O_NOCTTY|O_NDELAY|O_NONBLOCK);
@@ -42,3 +25,20 @@ void closeserialia(void)
 }
 int lege(void*p,int n){return read(portd,p,n);}
 int scribe(const void*p,int n){return write(portd,p,n);}
+/*hodo: POSIX serial port module
+This program is a part of the stribog auxiliary host software section
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+Copyright (C) 2006, 2007 D.Ineiev <ineiev@yahoo.co.uk>*/
