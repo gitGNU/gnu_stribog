3959ce38a91277757486690c1babe2de01293269
P.Kharitonov <ineiev@users.sourceforge.net>
2007-08-27 06:11:44 +0000

    the buffer for incoming symbols are used properly; hodo msmts output is 70 times per second when there is no GPS input and 50 times per second when there is some input

--- sw/auxilia/odo/hodo.c
+++ sw/auxilia/odo/hodo.c
@@ -1,35 +1,43 @@
 #include<avr/io.h>/*odometer firmware*/
-/*Fuse Low Byte = 0xef; Fuse High Byte = 0xc9; Fuse Extended Byte = 0xff*/
+/*atmega8 fuses:Low = 0xef; High = 0xc9; Extended = 0xff*/
 #define F_CPU   (14745600ul)
 #define BAUD    (9600)
-#define PERIOD	((unsigned char)(17))
 #define INPUT_MASK	(1<<5)
-static unsigned char capta[256],caput,cauda,missa[6],i=sizeof(missa);
-static unsigned cnt;
-static void init(void)
-{UBRRH=0;UBRRL=F_CPU/BAUD/16-1;UCSRB=(1<<RXEN)|(1<<TXEN);
- UCSRC=((1<<UCSZ1)|(1<<UCSZ0)|(1<<URSEL))/*8-bit,No parity*/;
- TCCR0=(1<<CS02)|(1<<CS00);/*clk_io/1024*/
- TCCR1B=(1<<CS12);/*clk_io/256*/
-}static void cape(void){if(!(UCSRA&(1<<RXC)))return;capta[caput++]=UDR;}
-static void mitte(void)
-{if(!(UCSRA&(1<<UDRE)))return;
- if(caput!=cauda)UDR=capta[cauda++];else
- {if(TCNT0<PERIOD&&!(TIFR&(1<<TOV0)))return;TCNT0=0;TIFR|=(1<<TOV0);
-  if(i<sizeof(missa))UDR=missa[i++];
- }
-}static void pone_mittenda(void)
-{unsigned t=TCNT1;
- missa[0]=(cnt>>6)|(3<<6);missa[sizeof(missa)-1]=missa[0];
+static unsigned char capta[668],missa[6],i=sizeof(missa);
+static unsigned cnt,caput,cauda;
+static void moram_iube(void){TCNT0=0;TIFR|=(1<<TOV0);}
+static unsigned char mora(void)
+{static const unsigned char period=170;return TCNT0<period&&!(TIFR&(1<<TOV0));}
+static void pone_mittenda(void)
+{unsigned t=TCNT1;missa[0]=(cnt>>6)|(3<<6);missa[sizeof(missa)-1]=missa[0];
  missa[1]=(((unsigned char)cnt)|(3<<6));missa[sizeof(missa)-1]^=missa[1];
  missa[2]=((unsigned char)(t>>12))|(3<<6);missa[sizeof(missa)-1]^=missa[2];
  missa[3]=((unsigned char)(t>>6))|(3<<6);missa[sizeof(missa)-1]^=missa[3];
  missa[4]=((unsigned char)t)|(3<<6);missa[sizeof(missa)-1]^=missa[4];
  missa[sizeof(missa)-1]&=~(1<<6);missa[sizeof(missa)-1]|=1<<7;i=0;
 }
+static void init(void)
+{UBRRH=0;UBRRL=F_CPU/BAUD/16-1;UCSRB=(1<<RXEN)|(1<<TXEN);
+ UCSRC=((1<<UCSZ1)|(1<<UCSZ0)|(1<<URSEL))/*8-bit,No parity*/;
+ TCCR0=(1<<CS02)|(1<<CS00);/*clk_io/1024*/
+ TCCR1B=(1<<CS12);/*clk_io/256*/
+ moram_iube();pone_mittenda();
+}
+static inline void ch_adde(unsigned char c)
+{capta[caput++]=c;if(caput>=sizeof(capta))caput=0;}
+static inline unsigned char ch_redde(void)
+{unsigned char c=capta[cauda++];if(cauda>=sizeof(capta))cauda=0;return c;}
+static unsigned char mittenda_redde(void)
+{if(i>=sizeof(missa))pone_mittenda();return missa[i++];}
+static void cape(void){if(!(UCSRA&(1<<RXC)))return;ch_adde(UDR);}
+static void mitte(void)
+{static unsigned char v,p=3;char _=caput!=cauda;if(!(UCSRA&(1<<UDRE)))return;
+ if(_){if(++v==p)v=0;}else{v=0;if(mora())return;if(i==sizeof(missa))moram_iube();}
+ UDR=v?ch_redde():mittenda_redde();
+}
 static void inc_cnt(void)
 {static unsigned char c1,c2;unsigned char c=PINC&INPUT_MASK;
- if(!(c1^c)&&(c2^c1))cnt++;c2=c1;c1=c;if(i==sizeof(missa))pone_mittenda();
+ if(!(c1^c)&&(c2^c1))cnt++;c2=c1;c1=c;
 }int main(void){init();while(1){inc_cnt();cape();mitte();}return 0;}
 /*This is an auxiliary part of stribog
 This program is free software; you can redistribute it and/or modify
