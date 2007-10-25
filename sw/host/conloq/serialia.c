2d8d22efed10652eef7e78320db163656386b353
P.Kharitonov <ineiev@users.sourceforge.net>
2007-10-25 07:37:14 +0000

    serial port mode is blocking with timeouts

--- sw/host/conloq/serialia.c
+++ sw/host/conloq/serialia.c
@@ -1,5 +1,31 @@
-/*conloq: POSIX serial port module
-This program is a part of the stribog host software section
+#include"serialia.h"/*conloq: POSIX serial port module*/
+#include<unistd.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<sys/ioctl.h>
+#include<sys/fcntl.h>
+#include<termios.h>
+#include<stdlib.h>
+#include<stdio.h>
+static struct termios vet;static char dv[]="/dev/ttyS1";
+static int portd=-1;
+int initserialia(const char*tty)
+{struct termios nov;portd=open(tty?tty:dv,O_RDWR|O_NOCTTY);
+ if(portd<0)return-1;tcgetattr(portd,&vet);nov=vet;
+ cfsetospeed(&nov,B115200);cfsetispeed(&nov,B115200);
+ nov.c_cflag|=CLOCAL|CREAD;nov.c_cflag&=~PARENB;
+ nov.c_cflag&=~CSIZE;nov.c_cflag|=CS8;nov.c_oflag&=~OPOST;
+ nov.c_lflag=nov.c_iflag=0;nov.c_cc[VMIN]=0;nov.c_cc[VTIME]=2;
+ tcsetattr(portd,TCSANOW,&nov);return portd<0;
+}
+void closeserialia(void)
+{if(0>portd)return;
+ tcsetattr(portd,TCSADRAIN,&vet);fcntl(portd,F_SETFL,0);
+ close(portd);portd=-1;
+}
+int lege(void*p,int n){return read(portd,p,n);}
+int scribe(const void*p,int n){return write(portd,p,n);}
+/*This source file is a part of the stribog host software section
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -14,31 +40,4 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-Copyright (C) 2006, 2007 D.Ineiev <ineiev@yahoo.co.uk>*/
-#include"serialia.h"
-#include<unistd.h>
-#include<sys/types.h>
-#include<sys/stat.h>
-#include<sys/ioctl.h>
-#include<fcntl.h>
-#include<termios.h>
-#include<stdlib.h>
-#include<stdio.h>
-static struct termios vet,nov;static char dv[]="/dev/ttyS1";
-static int portd=-1;
-int initserialia(const char*tty)
-{portd=open(tty?tty:dv,O_RDWR|O_NOCTTY|O_NDELAY|O_NONBLOCK);
- if(portd<0)return-1;tcgetattr(portd,&vet);nov=vet;
- cfsetospeed(&nov,B115200);cfsetispeed(&nov,B115200);
- nov.c_cflag|=CLOCAL|CREAD;nov.c_cflag&=~PARENB;
- nov.c_cflag&=~CSIZE;nov.c_cflag|=CS8;nov.c_oflag&=~OPOST;
- nov.c_lflag=nov.c_iflag=0;
- tcsetattr(portd,TCSANOW,&nov);fcntl(portd,F_SETFL,FNDELAY);
- return portd<0;
-}
-void closeserialia(void)
-{if(0>portd)return;
- tcsetattr(portd,TCSADRAIN,&vet);fcntl(portd,F_SETFL,0);close(portd);
-}
-int lege(void*p,int n){return read(portd,p,n);}
-int scribe(const void*p,int n){return write(portd,p,n);}
+Copyright (C) 2006, 2007 Ineiev<ineiev@users.sourceforge.net>*/
