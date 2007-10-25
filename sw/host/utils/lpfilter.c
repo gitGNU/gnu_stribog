c502c666e8beb5a45e268d52eaf0c6c060492aa5
P.Kharitonov <ineiev@users.sourceforge.net>
2007-10-25 07:42:57 +0000

    format string computation eliminated

--- sw/host/utils/lpfilter.c
+++ sw/host/utils/lpfilter.c
@@ -14,16 +14,16 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-Copyright (C) 2006 D.Ineiev <ineiev@yahoo.co.uk>*/
+Copyright (C) 2006, 2007 Ineiev<ineiev@users.sourceforge.net>*/
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-static int N=200,n,i,j;static double *s;static char fmt[0x11];
+static int N=200,n,i,j;static unsigned prec=2;static double*s;
 static void process_string(const char*u)
 {double x;int j;const char*_=u;while(*_==' ')++_;
  for(j=0;j<n;j++){sscanf(_,"%lf",&x);s[j]+=x;_=strchr(_+1,' ');}
  if(++i==N)
- {i=0;for(j=0;j<n;s[j++]=0)printf(fmt,s[j]/N);putchar('\n');}
+ {i=0;for(j=0;j<n;s[j++]=0)printf("%.*f ",prec,s[j]/N);putchar('\n');}
 }
 static void init_n(const char*u)
 {const char*_=u;while(*_==' ')++_;do{n++;_=strchr(_+1,' ');}while(_);
@@ -31,8 +31,9 @@ static void init_n(const char*u)
 }void free_all(void){if(s)free(s);}
 int main(int argc,char**argv)
 {static char str[4913];if(argc>1)sscanf(argv[1],"%i",&N);if(N<1)return 1;
- j=2;if(argc>2)sscanf(argv[2],"%i",&j);sprintf(fmt,"%%.%if ",j);
+ if(argc>2)sscanf(argv[2],"%u",&prec);
  fgets(str,sizeof(str),stdin);init_n(str);process_string(str);
- while(1){fgets(str,sizeof(str),stdin);if(feof(stdin))break;process_string(str);}
+ while(1)
+ {fgets(str,sizeof(str),stdin);if(feof(stdin))break;process_string(str);}
  free_all();return 0;
 }
