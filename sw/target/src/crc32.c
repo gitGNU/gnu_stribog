948a2fcebaaf0b923e73714bd856b735ce49a07c
P.Kharitonov <ineiev@users.sourceforge.net>
2007-10-25 08:14:06 +0000

    generalizing types to enable host-side code tests

--- sw/target/src/crc32.c
+++ sw/target/src/crc32.c
@@ -17,7 +17,7 @@ You should have received a copy of the GNU General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 Copyright (C) 2006 D.Ineiev <ineiev@yahoo.co.uk>*/
-unsigned form_crc(const unsigned*msg,unsigned n)
+unsigned long form_crc(const unsigned*msg,unsigned n)
 /*NB this works on 32-bit little-endian architectures only*/
 {unsigned crc=~0,j,k;const unsigned poly=0xEDB88320;
  while(n--)
--- sw/target/src/crc32.h
+++ sw/target/src/crc32.h
@@ -15,5 +15,6 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-Copyright (C) 2006 D.Ineiev <ineiev@yahoo.co.uk>*/
-unsigned form_crc(const unsigned*msg,unsigned n);
+Copyright (C) 2006, 2007 Ineiev <ineiev@users.sourceforge.net>*/
+typedef const unsigned*crc32_input_array_type;
+unsigned long form_crc(crc32_input_array_type msg,unsigned n);
