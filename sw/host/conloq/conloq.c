5d8aeda3c4aaea0282df46cd53c1180e98e92bbc
P.Kharitonov <ineiev@users.sourceforge.net>
2007-10-25 06:48:47 +0000

    SIGINT and SIGTERM decently processed; exit codes enumerated; sprintf() call replaced with snprintf()

--- sw/host/conloq/conloq.c
+++ sw/host/conloq/conloq.c
@@ -15,26 +15,42 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-Copyright (C) 2006 D.Ineiev <ineiev@yahoo.co.uk>*/
+Copyright (C) 2006, 2007 Ineiev<ineiev@users.sourceforge.net>*/
 #include"serialia.h"
 #include"usage.h"
 #include"parse_tsip.h"
 #include"exp.h"
 #include"error.h"
+#include<stdlib.h>
 #include<stdio.h>
+#include<signal.h>
+enum exit_codes{normal_exit=0,no_log_file=1,no_uart=2,unknown_sig=3};
 static FILE*next_file(void)
 {char s[289];int i=0;FILE*f;
- do{sprintf(s,"%iconloq.log",i++);f=fopen(s,"rt");if(f)fclose(f);}while(f);
+ do{snprintf(s,sizeof s,"%iconloq.log",i++);f=fopen(s,"rt");if(f)fclose(f);}
+ while(f);
  return fopen(s,"wb");
+}static tsip_buf*tb;static FILE*f;
+static void closeall(void)
+{closeserialia();close_exp();free_tsip(tb);tb=0;if(f)fclose(f);f=0;}
+static void sighunter(int sig)
+{int r=normal_exit;
+ switch(sig)
+ {case SIGINT:fprintf(stderr,"INTERRUPTED\n");break;
+  case SIGTERM:fprintf(stderr,"TERMINATED\n");break;
+  default:fprintf(stderr,"unregistered signum; exiting\n");r=unknown_sig;
+ }exit(r);
 }
 int main(int argc,char**argv)
-{tsip_buf*tb=new_tsip();int size,n,j,period=0x3F;const unsigned char*_;
- unsigned char s[0x121];FILE*f=next_file();
- if(!f){error("can't open log file\n");free_tsip(tb);return 1;}
+{int size,n,j,period=0x3F;const unsigned char*_;
+ unsigned char s[11520];f=next_file();atexit(closeall);
+ signal(SIGINT,sighunter);signal(SIGTERM,sighunter);
+ if(!f){error("can't open log file\n");return no_log_file;}
  if(argc>2)sscanf(argv[2],"%i",&period);init_exp(0,period);
+ if(argc>4)enable_escapes(!0);tb=new_tsip();
  if(initserialia(argc>1?argv[1]:0))
- {fclose(f);free_tsip(tb);close_exp();error("can't open serial port\n");return 2;}
+ {error("can't open serial port\n");return no_uart;}
  while(1)if(0<(n=lege(s,sizeof(s))))for(j=0;j<n;putc(s[j++],f))
   if((_=parse_tsip(tb,s[j],&size)))expone(_,size);
- closeserialia();close_exp();fclose(f);free_tsip(tb);return 0;
+ return normal_exit;
 }
