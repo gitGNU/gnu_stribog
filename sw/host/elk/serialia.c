d26eb94711f273446e69efcf122bc5be11cce51d
P.Kharitonov <ineiev@users.sourceforge.net>
2007-10-25 07:59:05 +0000

    nov struct storage class changed to auto; an unneeded fcntl() call removed

--- sw/host/elk/serialia.c
+++ sw/host/elk/serialia.c
@@ -1,20 +1,4 @@
-/*elk the LPC21x programmer: POSIX serial port module
-This program is a part of the stribog host software section
-
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program. If not, see <http://www.gnu.org/licenses/>.
-
-Copyright (C) 2006, 2007 D.Ineiev <ineiev@yahoo.co.uk>*/
+/*elk the LPC21x programmer: POSIX serial port module*/
 #include"serialia.h"
 #include<unistd.h>
 #include<sys/types.h>
@@ -24,7 +8,7 @@ Copyright (C) 2006, 2007 D.Ineiev <ineiev@yahoo.co.uk>*/
 #include<termios.h>
 #include<stdlib.h>
 #include<stdio.h>
-static struct termios vet,nov;static char dv[]="/dev/ttyS0";
+static struct termios vet;static char dv[]="/dev/ttyS0";
 static int portd=-1;
 int baud(int f)
 {if(f<14745+14745/20&&f>14745-14745/20)
@@ -43,18 +27,33 @@ int baud(int f)
  printf("unknown frequency: I'll try baud rate B9600\n");return B9600;
 }
 int initserialia(const char*tty,int freq)
-{portd=open(tty?tty:dv,O_RDWR|O_NOCTTY|O_NDELAY|O_NONBLOCK);
+{struct termios nov;portd=open(tty?tty:dv,O_RDWR|O_NOCTTY|O_NDELAY);
  if(portd<0)return-1;tcgetattr(portd,&vet);nov=vet;
  cfsetospeed(&nov,baud(freq));cfsetispeed(&nov,baud(freq));
  nov.c_cflag|=CLOCAL|CREAD;nov.c_cflag&=~PARENB;
  nov.c_cflag&=~CSIZE;nov.c_cflag|=CS8;nov.c_oflag&=~OPOST;
- nov.c_lflag=nov.c_iflag=0;
- tcsetattr(portd,TCSANOW,&nov);fcntl(portd,F_SETFL,FNDELAY);
- return portd<0;
+ nov.c_lflag=nov.c_iflag=0;tcsetattr(portd,TCSANOW,&nov);return portd<0;
 }
 void closeserialia(void)
 {if(0>portd)return;
- tcsetattr(portd,TCSADRAIN,&vet);fcntl(portd,F_SETFL,0);close(portd);
+ tcsetattr(portd,TCSADRAIN,&vet);fcntl(portd,F_SETFL,0);
+ close(portd);portd=-1;
 }
 int lege(void*p,int n){return read(portd,p,n);}
 int scribe(const void*p,int n){return write(portd,p,n);}
+/*This program is a part of the stribog host software section
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+Copyright (C) 2006, 2007 Ineiev<ineiev@users.sourceforge.net>*/
