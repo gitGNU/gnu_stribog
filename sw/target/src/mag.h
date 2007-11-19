9eaad2d0288de481e31b2e6227ab439b4197d740
P.Kharitonov <ineiev@users.sourceforge.net>
2007-11-19 10:06:37 +0000

    switched to the GPLv3

--- sw/target/src/mag.h
+++ sw/target/src/mag.h
@@ -2,7 +2,7 @@
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
+the Free Software Foundation; either version 3 of the License, or
 (at your option) any later version.
 
 This program is distributed in the hope that it will be useful,
@@ -13,5 +13,6 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-Copyright (C) 2006 D.Ineiev <ineiev@yahoo.co.uk>*/
+Copyright (C) 2006, 2007 Ineiev<ineiev@users.sourceforge.net>, super V 93*/
 void init_mag(void);const int*process_mag(const unsigned*adc);
+void set_mag_gains(int x,int y,int z);
