3010366d545adde51f67f8c1dfddbb5c39e621cb
P.Kharitonov <ineiev@users.sourceforge.net>
2007-11-19 10:12:33 +0000

    switched to the GPLv3; statistics output and preferences added

--- sw/target/src/multa.c
+++ sw/target/src/multa.c
@@ -3,7 +3,7 @@ This file is a part of stribog.
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
+the Free Software Foundation; either version 3 of the License, or
 (at your option) any later version.
 
 This program is distributed in the hope that it will be useful,
@@ -14,7 +14,7 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-Copyright (C) 2006, 2007 Ineiev<ineiev@users.sourceforge.net>*/
+Copyright (C) 2006, 2007 Ineiev<ineiev@users.sourceforge.net>, super V 93*/
 #include"uart1.h"
 #include"uart0.h"
 #include"led.h"
@@ -25,15 +25,16 @@ Copyright (C) 2006, 2007 Ineiev<ineiev@users.sourceforge.net>*/
 #include"adc.h"
 #include"accel.h"
 #include"mag.h"
-typedef enum
- {pps_length=1,temp_length=2,gps_data_length=3,fix_length=5}message_lengths;
+enum message_lengths
+{pps_length=1,temp_length=2,gps_data_length=3,stat_length=4,fix_length=5};
 static unsigned adxrs_temp[3],_2048;
-static void send_pps(void)
-{static int pps_got;static unsigned pps,k;const unsigned*_;
- if((_=get_pps())){pps=*_;pps_got=!0;k++;}
- if(pps_got)pps_got=send_fix(&pps,pps_length);
-}
-static void check_gps_data(void)
+static void 
+send_pps(void)
+{static int pps_got;static unsigned pps[pps_length],k;const unsigned*_;
+ if((_=get_pps())){pps[0]=*_;pps_got=!0;k++;}
+ if(pps_got)pps_got=send_fix(pps,pps_length);
+}static void 
+check_gps_data(void)
 {static unsigned _[gps_data_length];char*s=(char*)_;static int data_gotten;
  if(data_gotten){data_gotten=send_fix(_,gps_data_length);return;}
  if(!(data_gotten=receive0(s,sizeof(_)-1)))return;s[sizeof(_)-1]=data_gotten;
@@ -44,8 +45,8 @@ static void check_gps_data(void)
    rem=s+(sizeof _)-1;for(i=0;i<remainder;i++)*--rem=*ov++;
   }
  }data_gotten=send_fix(_,gps_data_length);
-}
-static void correct_adc(unsigned w[3],unsigned t[3],unsigned _2048)
+}static void 
+correct_adc(unsigned w[3],unsigned t[3],unsigned _2048)
 {static unsigned _8192,_,n,v,k;int i,d;static const int a0=641;
  _+=_2048;if(n++==3){v=!0;n=0;_8192=(_>>2)-a0;_=0;}
  if(v)d=_8192;else d=_2048-a0;k=((a0*a0-d*(a0+20)+d*d)*5+(42<<10));
@@ -75,15 +76,18 @@ static void process_accel(unsigned*out)
   else{out[j]=acc[j];spur[j]=0;}
  }if(i++>2)i=0;
 }*/
-static void process_accel(unsigned*out)
+static void 
+process_accel(unsigned*out)
 {int j;const unsigned*a;if(!(a=get_accel()))return;for(j=0;j<3;j++)out[j]=a[j];}
-static int send_msmt(void)
+static int adc_missed;
+static int 
+send_msmt(void)
 {static unsigned f[fix_length],a[3];const int*mag;unsigned*s=f,w[3];int t;
  const unsigned*adc;static int i,pending;static int b[3];
- if(pending){pending=send_fix(f,fix_length);/*if(!pending){led1_set();led1_clr();}*/}
+ if(pending){pending=send_fix(f,fix_length);}
  if(!(adc=get_adc()))return 0;_2048=adc[ADC_2048];
  adxrs_temp[0]=adc[ADC_TX];adxrs_temp[1]=adc[ADC_TY];adxrs_temp[2]=adc[ADC_TZ];
- if((mag=process_mag(adc))){if(i++&3)return 0;}else return 0;
+ if(!(mag=process_mag(adc)))return 0;if(3&++i)return 0;if(pending)adc_missed++;
  *b=*mag;b[1]=mag[1];b[2]=mag[2];t=get_temperature();
  w[0]=adc[ADC_WX];w[1]=adc[ADC_WY];w[2]=adc[ADC_WZ];
  correct_adc(w,adxrs_temp,_2048);process_accel(a);
@@ -91,20 +95,48 @@ static int send_msmt(void)
  *s++=(b[2]&0xFFF)|((a[0]&0x3FFF)<<12)|((t&0x1F00)<<(26-8));
  *s++=(a[1]&0x3FFF)|((a[2]&0x3FFF)<<14);
  *s=(w[0]&0x3FF)|((w[1]&0x3FF)<<10)|((w[2]&0x3FF)<<20);
- pending=send_fix(f,fix_length);/*if(!pending){led1_set();led1_clr();}//ne micet*/
- return!0;
-}
-static void send_temp(int ms)
-{static unsigned t[2],i;unsigned*s=t;static const unsigned N=100;
+ pending=send_fix(f,fix_length);return!0;
+}static void 
+send_temp(int ms)
+{static unsigned t[temp_length],i;unsigned*s=t;static const unsigned N=100;
  if(ms)i++;if(i<N)return;*s++=_2048;
  *s=adxrs_temp[0]|(adxrs_temp[1]<<10)|(adxrs_temp[2]<<20);
  if(!send_fix(t,temp_length))i=0;
-}
-int main(void)
-{int j=0,ms;start_pll();init_power();init_led();init_lm74();init_accel();//led1_clr();
- init_mag();connect_pll();init_adc();init_uart1();init_uart0();init_tempus();
+}static void
+parse_settings(const unsigned char*s,int n)
+{int kx,ky=0,kz=0,acc_prescaler=9;
+ if(n>0)kx=*s;if(n>1)ky=s[1];if(n>2)kz=s[2];set_mag_gains(kx,ky,kz);
+ if(n>3)acc_prescaler=s[3];init_accel(acc_prescaler);
+}static void
+init_all(void)
+{unsigned char settings[0x11];int n;
+ start_pll();init_power();init_led();init_lm74();
+ init_mag();connect_pll();init_adc();init_uart1();//led1_set();
+ init_tempus();n=init_uart0(settings,sizeof(settings));
+ if(n>0)parse_settings(settings,n);
+}enum multa_constants
+{ushort_bits=16,ushort_mask=(1<<ushort_bits)-1,
+ tmin_initial=ushort_mask
+};
+static unsigned tmax,tmin=tmin_initial,tsum,tsum_N;
+static void 
+send_stat(int ms)
+{static unsigned st[stat_length],i;static const unsigned N=400;
+ if(ms)i++;if(i<N)return;
+ if(!st[0])
+ {st[0]=tmax|(tmin<<ushort_bits);
+  st[1]=(adc_missed&ushort_mask)|((uart0_overflows<<16)&ushort_mask);
+  st[2]=(accel_error_count&ushort_mask)|((tsum>>ushort_bits)<<ushort_bits);
+  st[3]=tsum_N;tsum=tmax=tsum_N=0;tmin=tmin_initial;
+ }if(!send_fix(st,stat_length))st[0]=i=0;
+}int 
+main(void)
+{int j=0,ms;init_all();
  while(1)
- {ms=send_msmt();send_temp(ms);send_pps();check_gps_data();
+ {unsigned t=iunius_tempus();
+  ms=send_msmt();send_temp(ms);send_pps();check_gps_data();send_stat(ms);
   if((j++&(1<<15)))led0_set();else led0_clr();
+  t=iunius_tempus()-t;if(t>tmax)tmax=t;if(tmin>t)tmin=t;
+  tsum+=t;tsum_N++;
  }return 0;
 }
