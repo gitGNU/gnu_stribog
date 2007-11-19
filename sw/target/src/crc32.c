c5dccbf99b0756c4561739dbfbdca7c65fd8a541
P.Kharitonov <ineiev@users.sourceforge.net>
2007-11-19 10:18:04 +0000

    switched to the GPLv3; slightly different types

--- sw/target/src/crc32.c
+++ sw/target/src/crc32.c
@@ -1,11 +1,17 @@
-/*CRC32(CCITT-32): adjusted to 32-bit words algorithm from
+/*CRC32(CCITT-32): adjusted to 32-bit little-endian words algorithm from
 Robert L. Hummel 
-Programmer's Technical Reference: Data and Fax Communication
-This file has been written for the stribog project.
+Programmer's Technical Reference: Data and Fax Communication*/
+#include"crc32.h"
+unsigned long form_crc(const crc32_input_array_token*msg,unsigned n)
+{unsigned crc=~0,j,k;const unsigned poly=0xEDB88320;
+ while(n--)
+ {crc^=*msg++;for(j=0;j<32;j++){k=crc&1;crc>>=1;if(k)crc^=poly;}}
+ return~crc;
+}/*This file has been written for the stribog project.
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
+the Free Software Foundation; either version 3 of the License, or
 (at your option) any later version.
 
 This program is distributed in the hope that it will be useful,
@@ -16,11 +22,4 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-Copyright (C) 2006 D.Ineiev <ineiev@yahoo.co.uk>*/
-unsigned long form_crc(const unsigned*msg,unsigned n)
-/*NB this works on 32-bit little-endian architectures only*/
-{unsigned crc=~0,j,k;const unsigned poly=0xEDB88320;
- while(n--)
- {crc^=*msg++;for(j=0;j<32;j++){k=crc&1;crc>>=1;if(k)crc^=poly;}}
- return~crc;
-}
+Copyright (C) 2006, 2007 Ineiev <ineiev@users.sourceforge.net>, super V 93*/
--- sw/target/src/crc32.h
+++ sw/target/src/crc32.h
@@ -1,10 +1,13 @@
 /*CRC32(CCITT-32) checksum
-NB current implementation works on 32-bit little-endian architectures only
-This file has been written for the stribog project.
+NB: this implementation tailored for and works on 
+32-bit little-endian architectures only.*/
+typedef unsigned crc32_input_array_token;
+unsigned long form_crc(const crc32_input_array_token*msg,unsigned n);
+/*This file is a part of the stribog project.
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
+the Free Software Foundation; either version 3 of the License, or
 (at your option) any later version.
 
 This program is distributed in the hope that it will be useful,
@@ -15,6 +18,4 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-Copyright (C) 2006, 2007 Ineiev <ineiev@users.sourceforge.net>*/
-typedef const unsigned*crc32_input_array_type;
-unsigned long form_crc(crc32_input_array_type msg,unsigned n);
+Copyright (C) 2006, 2007 Ineiev <ineiev@users.sourceforge.net>, super V 93*/
