bdebee13679383be1d324a5d66185d162164749c
P.Kharitonov <ineiev@users.sourceforge.net>
2007-11-19 10:03:54 +0000

    switched to GPLv3; settings reception on init added

--- sw/target/src/uart0.h
+++ sw/target/src/uart0.h
@@ -3,7 +3,7 @@ This file has been written for the stribog project.
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
+the Free Software Foundation; either version 3 of the License, or
 (at your option) any later version.
 
 This program is distributed in the hope that it will be useful,
@@ -14,6 +14,6 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-Copyright (C) 2006, 2007 Ineiev<ineiev@users.sourceforge.net>*/
-int init_uart0(void);int receive0(char*d,int n);
+Copyright (C) 2006, 2007 Ineiev<ineiev@users.sourceforge.net>, super V 93*/
+int init_uart0(unsigned char*settings,int size);int receive0(char*d,int n);
 int ask_ephm(void);extern int uart0_overflows;
