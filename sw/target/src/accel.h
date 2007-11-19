41dc5889ae300eb2a8d42524ff609480e391f1a5
P.Kharitonov <ineiev@users.sourceforge.net>
2007-11-19 10:27:25 +0000

    switched to the GPLv3; prescaler made selectable on init; accelerometer errors count added;

--- sw/target/src/accel.h
+++ sw/target/src/accel.h
@@ -3,7 +3,7 @@ This file is a part of stribog.
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
+the Free Software Foundation; either version 3 of the License, or
 (at your option) any later version.
 
 This program is distributed in the hope that it will be useful,
@@ -14,5 +14,6 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-Copyright (C) 2006 D.Ineiev <ineiev@yahoo.co.uk>*/
-void init_accel(void);const unsigned*get_accel(void);
+Copyright (C) 2006, 2007 Ineiev<ineiev@users.sourceforge.net>, super V 93*/
+void init_accel(int timer0_prescaler);const unsigned*get_accel(void);
+extern unsigned accel_error_count;
