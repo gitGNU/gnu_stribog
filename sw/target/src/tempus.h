4e2c3a12f1014829f57ce8567900c5b7fe5ec172
P.Kharitonov <ineiev@users.sourceforge.net>
2007-11-19 10:07:31 +0000

    switched to the GPLv3; iunius_tempus() went here for efficiency

--- sw/target/src/tempus.h
+++ sw/target/src/tempus.h
@@ -4,7 +4,7 @@ This file has been written for the stribog project.
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
+the Free Software Foundation; either version 3 of the License, or
 (at your option) any later version.
 
 This program is distributed in the hope that it will be useful,
@@ -15,7 +15,13 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-Copyright (C) 2006 D.Ineiev <ineiev@yahoo.co.uk>*/
+Copyright (C) 2006, 2007 Ineiev<ineiev@users.sourceforge.net>, super V 93*/
 void init_tempus(void);unsigned long long tempus(void);
-inline unsigned senius_tempus(void);inline unsigned iunius_tempus(void);
+unsigned senius_tempus(void);
+static inline unsigned 
+iunius_tempus(void)
+{
+#define T1TC_pro_iuniore_tempore (*((volatile unsigned*)(0xE0008008)))
+ return T1TC_pro_iuniore_tempore;
+}
 const unsigned*get_pps(void);
