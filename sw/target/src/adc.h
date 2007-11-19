6360f0a55443b097cce6b43fe8bd19c6687a3c20
P.Kharitonov <ineiev@users.sourceforge.net>
2007-11-19 10:19:45 +0000

    switched to the GPLv3; defines replaced with enum

--- sw/target/src/adc.h
+++ sw/target/src/adc.h
@@ -1,9 +1,9 @@
 /*ADC input
-This file has been written for the stribog project.
+This file is a part the stribog project.
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
+the Free Software Foundation; either version 3 of the License, or
 (at your option) any later version.
 
 This program is distributed in the hope that it will be useful,
@@ -14,15 +14,11 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-Copyright (C) 2006 D.Ineiev <ineiev@yahoo.co.uk>*/
-#define ADC_MX		(4)
-#define ADC_MY		(5)
-#define ADC_MZ		(0)
-#define ADC_WX		(2)
-#define ADC_WY		(3)
-#define ADC_WZ		(1)
-#define ADC_TX		(10)
-#define ADC_TY		(15)
-#define ADC_TZ		(7)
-#define ADC_2048	(14)
+Copyright (C) 2006, 2007 Ineiev<ineiev@users.sourceforge.net>, super V 93*/
+enum{
+ ADC_MX=4, ADC_MY=5, ADC_MZ=0,
+ ADC_WX=2, ADC_WY=3, ADC_WZ=1,
+ ADC_TX=10,ADC_TY=15,ADC_TZ=7,
+ ADC_2048=14
+};
 void init_adc(void);const unsigned*get_adc(void);
