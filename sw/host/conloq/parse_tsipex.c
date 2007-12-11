39cc8072b43248f7136cae6c6e21864fa1f2545b
P.Kharitonov <ineiev@users.sourceforge.net>
2007-12-11 06:34:38 +0000

    general cleanup
    function names in definitions reformatted
    as GNU coding standards propose;
    get_u removed to get_u.h

--- sw/host/conloq/parse_tsipex.c
+++ sw/host/conloq/parse_tsipex.c
@@ -1,21 +1,21 @@
 #include"parse_tsip.h"/*parse_tsipex: parse_tsip module test*/
 #include"error.h"
-#include<stdint.h>
 #include"crc32.h"
 #define hostside_form_fix
 #include"../../common/form_fix.h"
+#include"get_u.h"
 #include<stdio.h>
 #include<stdlib.h>
 static unsigned long long bytes_processed;
 crc32_input_array_token buf[0x121];int message_length;
-int lege(unsigned char*s,int n)
+int
+lege(unsigned char*s,int n)
 {int len,i;message_length=(1+(random()%5))<<2;
  if(n<(message_length+4)*2+4)return 0;
  for(i=0;i<message_length;i++)buf[i]=random();
  form_fix(buf,message_length>>2,(char*)s,&len);bytes_processed+=len;return len;
-}uint32_t get_u(const unsigned char*s)
-{return*s|(((uint32_t)s[1])<<8)|(((uint32_t)s[2])<<16)|(((uint32_t)s[3])<<24);}
-void expone(const unsigned char*s,int size)
+}void
+expone(const unsigned char*s,int size)
 {static unsigned long long u;int i;++u;
  unsigned long crc,cr;crc=form_crc(s,(size>>2)-1);cr=get_u(s+size-4);
  if(crc!=cr)
@@ -29,7 +29,8 @@ void expone(const unsigned char*s,int size)
    u>>10,bytes_processed>>10);
   fflush(stdout);
  }
-}int main(int argc,char**argv)
+}int
+main(int argc,char**argv)
 {tsip_buf*tb;int size,n,j;const unsigned char*_;unsigned char s[0x121];
  if(argc>1)
  {printf("escapes enabled\n");enable_escapes(!0);enable_push_esc=!0;}
@@ -53,4 +54,4 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-Copyright (C) 2007 Ineiev <ineiev@users.sourceforge.net>, super V 93*/
+Copyright (C) 2007 Ineiev<ineiev@users.sourceforge.net>, super V 93*/
