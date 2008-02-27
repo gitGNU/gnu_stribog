f5bfeaf97041fb89b7f001423da56b95f41d6173
P.Kharitonov <ineiev@users.sourceforge.net>
2008-02-27 20:26:27 +0300

    honour verbous modes

--- sw/host/conloq/serialia.c
+++ sw/host/conloq/serialia.c
@@ -1,29 +1,7 @@
-#include"serialia.h"/*conloq: POSIX serial port module*/
-#include<unistd.h>
-#include<sys/fcntl.h>
-#include<termios.h>
-#include<stdio.h>
-static struct termios vet;/* previous port settings */
-static char dv[]="/dev/ttyS1";/* default device name */
-static int port=-1;/* file descriptor */
-int
-initserialia(const char*tty)
-{struct termios nov;port=open(tty?tty:dv,O_RDWR|O_NOCTTY);
- if(port<0)return-1;tcgetattr(port,&vet);nov=vet;
- cfsetospeed(&nov,B115200);cfsetispeed(&nov,B115200);
- nov.c_cflag|=CLOCAL|CREAD;nov.c_cflag&=~PARENB;
- nov.c_cflag&=~CSIZE;nov.c_cflag|=CS8;nov.c_oflag&=~OPOST;
- nov.c_lflag=nov.c_iflag=0;nov.c_cc[VMIN]=0;nov.c_cc[VTIME]=2;
- tcsetattr(port,TCSANOW,&nov);return port<0;
-}void
-closeserialia(void)
-{if(0>port)return;tcsetattr(port,TCSANOW,&vet);
- fcntl(port,F_SETFL,0);close(port);port=-1;
-}int
-lege(void*p,int n){return read(port,p,n);}
-int
-scribe(const void*p,int n){return write(port,p,n);}
-/*This source file is a part of the stribog host software section
+/*POSIX serial port module
+Copyright (C) 2006, 2007, 2008\
+ Ineiev<ineiev@users.sourceforge.net>, super V 93
+This file is a part of the stribog host software section
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -36,6 +14,38 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with this program. If not, see <http://www.gnu.org/licenses/>.
+along with this program. If not, see <http://www.gnu.org/licenses/>.*/
+#include"serialia.h"
+#include"verbosity_level.h"
+#include<unistd.h>
+#include<sys/fcntl.h>
+#include<termios.h>
+#include<stdio.h>
+static struct termios vet;/* previous port settings */
+static const char*dv="/dev/ttyS1";/* default device name */
+static int port=-1;/* file descriptor */
 
-Copyright (C) 2006, 2007 Ineiev<ineiev@users.sourceforge.net>, super V 93*/
+int
+initserialia(const char*tty)
+{struct termios nov;if(tty)dv=tty;
+ port=open(dv,O_RDWR|O_NOCTTY);
+ if(get_verbosity()>=pretty_verbose)printf("UART device \"%s\": ",dv);
+ if(port<0)
+ {if(get_verbosity()>=pretty_verbose)printf("failed to open\n");return-1;}
+ tcgetattr(port,&vet);nov=vet;
+ cfsetospeed(&nov,B115200);cfsetispeed(&nov,B115200);
+ nov.c_cflag|=CLOCAL|CREAD;nov.c_cflag&=~PARENB;
+ nov.c_cflag&=~CSIZE;nov.c_cflag|=CS8;nov.c_oflag&=~OPOST;
+ nov.c_lflag=nov.c_iflag=0;nov.c_cc[VMIN]=0;nov.c_cc[VTIME]=2;
+ tcsetattr(port,TCSANOW,&nov);
+ if(get_verbosity()>=pretty_verbose)
+ {printf("%s\n",port<0?"fail":"success");}
+ return port<0;
+}void
+closeserialia(void)
+{if(0>port)return;tcsetattr(port,TCSANOW,&vet);
+ fcntl(port,F_SETFL,0);close(port);port=-1;
+}int
+lege(void*p,int n){return read(port,p,n);}
+int
+scribe(const void*p,int n){return write(port,p,n);}
