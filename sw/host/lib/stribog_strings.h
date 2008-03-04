09ca00c467ac2aef36e01c5c314a454e8962151e
P.Kharitonov <ineiev@users.sourceforge.net>
2008-03-04 15:11:15 +0300

    new

---
+++ sw/host/lib/stribog_strings.h
@@ -0,0 +1,26 @@
+/*commonly used strings
+Copyright (C) 2008\
+ Ineiev<ineiev@users.sourceforge.net>, super V 93
+This program is a part of the stribog host software section
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.*/
+
+#define PACKAGE_VERSION_COMMENTED " ("PACKAGE_NAME") "\
+ PACKAGE_VERSION"\n"\
+"Copyright (C) 2008 Ineiev<ineiev@users.sourceforge.net>, super V 93\n"\
+"stribog comes with NO WARRANTY, to the extent permitted by law.\n"\
+"You may redistribute copies of stribog\n"\
+"under the terms of the GNU General Public License V3+.\n"\
+"For more information about these matters,\n"\
+"see <http://www.gnu.org/licenses/>."
