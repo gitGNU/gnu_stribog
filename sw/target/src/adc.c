621a33930eb321418d0a176e53e57b2a11049ad4
P.Kharitonov <ineiev@users.sourceforge.net>
2008-03-21 17:54:23 +0300

    ADC1 presence is configured

--- sw/target/src/adc.c
+++ sw/target/src/adc.c
@@ -15,6 +15,13 @@ GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.*/
+#if HAVE_CONFIG_H
+ #include<config.h>
+#else
+ #ifndef HAVE_ADC1
+  #define HAVE_ADC1 1
+ #endif
+#endif
 #include"adc.h"
 #include"dac.h"
 #include"../include/lpc2138.h"
@@ -22,27 +29,37 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.*/
 #define CHANNELS	(16)
 static unsigned channels[CHANNELS],*ch0=channels,*ch1=channels+8;
 static mutex data_read;
-const unsigned*get_adc(void){if(lock(&data_read))return 0;return channels;}
+const unsigned*
+get_adc(void){if(lock(&data_read))return 0;return channels;}
 static void qu0(void)__attribute__((interrupt("IRQ")));
-static void qu0(void)
+static void
+qu0(void)
 {unsigned c,cn;static int j;
  while((c=AD0DR)&AD_DONE)
- {ch0[cn=(c>>24)&7]=(c>>6)&0x3FF;if(cn==7){if(j!=4)j++;else{j=0;unlock(&data_read);}}}
- VICVectAddr=0;
+ {ch0[cn=(c>>24)&7]=(c>>6)&0x3FF;
+  if(cn==7){if(j!=4)j++;else{j=0;unlock(&data_read);}}
+ }VICVectAddr=0;
 }
 static void qu1(void)__attribute__((interrupt("IRQ")));
-static void qu1(void)
+static void
+qu1(void)
 {unsigned c;while((c=AD1DR)&AD_DONE)ch1[(c>>24)&7]=(c>>6)&0x3FF;VICVectAddr=0;}
-void init_adc(void)
+void
+init_adc(void)
 {init_dac();
  VICVectAddr6=(unsigned)qu0;VICVectCntl6=VIC_CntlEnable|VIC_AD0;
- VICVectAddr5=(unsigned)qu1;VICVectCntl5=VIC_CntlEnable|VIC_AD1;
- VICIntEnable=(1<<VIC_AD0)|(1<<VIC_AD1);
- PINSEL0&=PINSEL0_AD12MASK&PINSEL0_AD07MASK;PINSEL0|=PINSEL0_AD07|PINSEL0_AD12;
+ if(HAVE_ADC1)
+ {VICVectAddr5=(unsigned)qu1;VICVectCntl5=VIC_CntlEnable|VIC_AD1;}
+ VICIntEnable=(1<<VIC_AD0)|(HAVE_ADC1?(1<<VIC_AD1):0);
+ PINSEL0&=PINSEL0_AD12MASK&PINSEL0_AD07MASK;
+ PINSEL0|=PINSEL0_AD07|(HAVE_ADC1?PINSEL0_AD12:0);
  PINSEL1&=PINSEL1_AD00MASK&PINSEL1_AD01MASK&PINSEL1_AD02MASK&
-  PINSEL1_AD03MASK&PINSEL1_AD04MASK&PINSEL1_AD05MASK&PINSEL1_AD17MASK;
+  PINSEL1_AD03MASK&PINSEL1_AD04MASK&PINSEL1_AD05MASK&
+  PINSEL1_AD16MASK&PINSEL1_AD17MASK;
  PINSEL1|=PINSEL1_AD00|PINSEL1_AD01|PINSEL1_AD02|
-  PINSEL1_AD03|PINSEL1_AD04|PINSEL1_AD05|PINSEL1_AD16|PINSEL1_AD17;
+  PINSEL1_AD03|PINSEL1_AD04|PINSEL1_AD05|
+   (HAVE_ADC1?(PINSEL1_AD16|PINSEL1_AD17):0);
  AD0CR=(0xBF)|(11<<8)|AD_PDN|AD_BURST;
- AD1CR=(0xC4)|(33<<8)|AD_PDN|AD_BURST;lock(&data_read);
+ if(HAVE_ADC1)AD1CR=(0xC4)|(33<<8)|AD_PDN|AD_BURST;
+ lock(&data_read);
 }
