31df9fdcf153d8957b2592efeaa5a54d1ade604a
P.Kharitonov <ineiev@users.sourceforge.net>
2008-03-25 21:42:23 +0300

    approaching to GNU-standard arg parsing

--- sw/host/elk/elk.c
+++ sw/host/elk/elk.c
@@ -15,15 +15,7 @@ GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.*/
-#if HAVE_CONFIG_H
- #include<config.h>
-#endif
-#ifndef PACKAGE_DATA_DIR
- #define package_data_dir "."
- #define PACKAGE_DATA_DIR "."
-#else
- #define package_data_dir PACKAGE
-#endif
+#include<config.h>
 #include<stdlib.h>
 #include<stdio.h>
 #include"serialia.h"
@@ -32,11 +24,19 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.*/
 #include<string.h>
 #include<time.h>
 #include<stdarg.h>
-int 
+#include<argp.h>
+#include<stribog_strings.h>
+#include<stribog_error.h>
+const char prefs_file_name[]=".elk.prefs.rc";
+enum program_exit_codes
+{normal_exit=0,no_uart=2,no_directory=3,wrong_args=4};
+static void
+drain_uart(void){char s[0x11];while(0<lege(s,sizeof s));}
+enum program_exit_codes 
 init(const char*s,int f)
-{int err=0;printf("setting up port \"%s\"...",s);
- if(init_serialia(s,f)){err=-1;printf(" failed\n");}
- else printf("port setup ok\n");return err;
+{printf("setting up port \"%s\"...",s);
+ if(init_serialia(s,f)){printf(" failed\n");return no_uart;}
+ printf("port setup ok\n");drain_uart();return normal_exit;
 }/*NB (24 Oct 2007) it is not good, but the serial port is setup 
  without no timeouts, so the programme is polling CPU cycles, 
  therefore clock() difference will show something not too far 
@@ -48,8 +48,6 @@ wait_for_chars(char*s,int N,int timeout)
  do{n=lege(_,N-i);if(n>0){i+=n;_+=n;}}
  while(i<N&&clock()-t<timeout*CLOCKS_PER_SEC/5);return i;
 }
-void
-drain_uart(void){char s[0x11];while(0<lege(s,sizeof s));}
 const char*
 code_string(int n)
 {switch(n)
@@ -79,7 +77,7 @@ int
 test_code(const char*s)
 {int n=-1;const char*cs=0;if(sscanf(s,"%i",&n)==1)cs=code_string(n);
  if(n||!mute)
- {if(NULL!=cs)printf("%s\n",cs);else printf("unknown return code %s\n",s);}
+ {if(cs)printf("%s\n",cs);else printf("unknown return code %s\n",s);}
  switch(n)
  {case 0:if(!mute)printf("CMD_SUCCESS\n");break;
   case 1:printf("INVALID_COMMAND\n");break;
@@ -126,14 +124,14 @@ unlock(void)
 }
 #define snprintf_checked(s,size,fmt,...) output_snprintf_error(\
  __FILE__,__LINE__,s,size,fmt,__VA_ARGS__)
+#ifdef __GNUC__
 int output_snprintf_error(const char*file,int line,char*s,unsigned size,
  const char*fmt,...)
-#ifdef __GNUC__
 __attribute__((format(printf,5,6)));
+#endif
 int 
 output_snprintf_error(const char*file,int line,char*s,unsigned size,
  const char*fmt,...)
-#endif
 {va_list ap;int result;
  va_start(ap,fmt);result=vsnprintf(s,size,fmt,ap);va_end(ap);
  if(result<0||result>=size)
@@ -142,8 +140,14 @@ output_snprintf_error(const char*file,int line,char*s,unsigned size,
    fprintf(stderr,"too short buffer (%i chars, %i needed)\n",size,result);
   if(result<0)fprintf(stderr,"conversion error %i\n",result);return!0;
  }return 0;
-}int
-synchronize(int f)
+}
+struct arguments
+{const char*port_name,*target_name,
+  *directory,*package_data_dir;
+ int verbosity,file_input,run,write,freq;
+}args;
+int
+synchronize(void)
 {const char query[]="?",sy[]="Synchronized\r\n",
   ok[]="Synchronized\r\nOK\r\n";char s[289],s0[289];int n,i=0,vex;
  drain_uart();
@@ -154,10 +158,11 @@ synchronize(int f)
  }while(strcmp(sy,s));
  scribe(sy,sizeof(sy)-1);printf("Synchronized sent; ");
  n=wait_for_chars(s,18,1);s[n]=0;printf("received %i bytes: %s",n,s);
- if(strcmp(ok,s))return!0;if(snprintf_checked(s,sizeof s,"%i\r\n",f))return!0;
- printf("sent %i; ",f);scribe(s,strlen(s));i=strlen(s)+4;
+ if(strcmp(ok,s))return!0;
+ if(snprintf_checked(s,sizeof s,"%i\r\n",args.freq))return!0;
+ printf("sent %i; ",args.freq);scribe(s,strlen(s));i=strlen(s)+4;
  n=wait_for_chars(s,i,2);s[n]=0;printf("received %i bytes: %s",n,s);
- if(snprintf_checked(s0,sizeof s0,"%i\r\nOK\r\n",f))return!0;
+ if(snprintf_checked(s0,sizeof s0,"%i\r\nOK\r\n",args.freq))return!0;
  if(!(vex=strcmp(s0,s)))echo_off();unlock();read_partid();return vex;
 }void
 read_version(void)
@@ -277,9 +282,8 @@ cache_checksum(FILE*f)
 {int i;
  for(i=0;i<sizeof vectors;i++)vectors[i]=getc(f);
  if(feof(f))
- {fprintf(stderr,"%s:%i: too short ARM program file\n",__FILE__,__LINE__);
-  return!0;
- }put_cksum(vectors);return 0;
+ {error("too short ARM program file\n");return!0;}
+ put_cksum(vectors);return 0;
 }
 int
 copy_memory(unsigned long addr)
@@ -293,15 +297,14 @@ copy_memory(unsigned long addr)
  printf("received %i bytes: %s",n,s);return test_code(s);
 }
 static int
-write_file(const char*file_name)
-{char s[289];FILE*f=fopen(file_name,"rb");int i,n=0;unsigned long addr=ram;
- if(!f)
- {fprintf(stderr,"%s:%i: can't open file '%s'\n",__FILE__,__LINE__,file_name);
-  return!0;
- }
+write_file(void)
+{char s[289];FILE*f;int i,n=0;unsigned long addr=ram;
+ /*it is impractical to have longer target names*/
+ strncpy(s,args.target_name,sizeof s);strncat(s,"-rom.bin",sizeof s);
+ f=fopen(s,"rb");
+ if(!f){error("can't open file '%s'\n",s);return!0;}
  if(cache_checksum(f))
- {fprintf(stderr,"%s:%i: error while caching checksum for '%s' file\n",
-   __FILE__,__LINE__,file_name);
+ {error("error while caching checksum for '%s' file\n",s);
   fclose(f);return!0;
  }
  for(addr=ram,i=0;i<sizeof vectors;i+=step,addr+=step)
@@ -340,19 +343,22 @@ reset_prefs(void)
  printf("can't reset connection\n");return!0;
 }void
 send_empty_prefs(void){char c[2]={reset,end};scribe(c,2);}
-static int no_preferences;
+static int no_preferences;static FILE*preferences_file;
 int
 load_prefs(void)
-{const char format[]="mag=%i,%i,%i\nacc=%i",prefs_file[]="prefs.dat";
+{const char format[]="mag=%i,%i,%i\nacc=%i";
  enum{prefs_num=4/*NB this depends on format*/};char c[prefs_num+1],s[0x11];
- int n,x[prefs_num],i;FILE*f;
- if(no_preferences)return 0;f=fopen(prefs_file,"r");
- if(NULL==f)
- {printf("no file \"%s\"",prefs_file);send_empty_prefs();return 0;}
- if(prefs_num!=fscanf(f,format,x,x+1,x+2,x+3))
- {fprintf(stderr,"%s:%i: wrong preferences format\n",__FILE__,__LINE__);
-  fprintf(stderr,"%s:%i: should be \"%s\"\n",__FILE__,__LINE__,format);return!0;
- }if(x[3]<=0)x[3]=9;c[(sizeof c)-1]=end;for(i=0;i<sizeof x/sizeof*x;i++)c[i]=x[i];
+ int n,x[prefs_num],i;if(no_preferences)return 0;
+ if(!preferences_file)
+ {error("no preferences file; sending empty preferences\n");
+  send_empty_prefs();return 0;
+ }
+ if(prefs_num!=fscanf(preferences_file,format,x,x+1,x+2,x+3))
+ {error("wrong preferences format\n");
+  error("should be \"%s\"\n",format);return!0;
+ }fclose(preferences_file);preferences_file=0;
+ if(x[3]<=0)x[3]=9;c[(sizeof c)-1]=end;
+ for(i=0;i<sizeof x/sizeof*x;i++)c[i]=x[i];
  for(i=0;i<0x11;i++)
  {if(reset_prefs())return!0;
   scribe(c,(sizeof c)-1);printf("sent:    ");print_array(c,(sizeof c)-1);
@@ -367,19 +373,22 @@ load_prefs(void)
    }if(n>0){printf("nack:");print_array(s,n);}
   }
  }return 0;
-}int
+}
+int
 load_and_go(void)
 {static const unsigned long ram_org=0x40000000,used_ram=0x40000200;
  int i,m=mute;FILE*f;unsigned long addr;char s[289];mute=1;
- if(!(f=fopen("vectors","rb")))
- {printf("no interrupt vectors in current directory\n");return !0;}
+ strncpy(s,args.target_name,sizeof s);strncat(s,".vectors",sizeof s);
+ if(!(f=fopen(s,"rb")))
+ {printf("no interrupt vectors file found\n");return!0;}
  for(addr=ram_org;!feof(f);addr+=step)
  {for(i=0;i<step&&!feof(f);i++)fscanf(f,"%c",s+i);
   printf("loading %i bytes at addr=0x%lX",i,addr);
   if(write_string(s,addr,step)){fclose(f);return!0;}putchar('\n');
  }fclose(f);
- if(!(f=fopen("elk.bin","rb")))
- {printf("no programme in current directory\n");return !0;}
+ strncpy(s,args.target_name,sizeof s);strncat(s,".bin",sizeof s);
+ if(!(f=fopen(s,"rb")))
+ {printf("no programme found\n");return!0;}
  for(addr=used_ram;!feof(f);addr+=step)
  {for(i=0;i<step;i++)fscanf(f,"%c",s+i);
   printf("loading %i bytes at addr=0x%lX",i,addr);
@@ -388,43 +397,138 @@ load_and_go(void)
 }int
 copy_mem(void){return copy_memory(0);}
 void
-close_all(void){close_serialia();}
-void
-program_ram(int f)
-{clock_t t;synchronize(f);t=clock();while(clock()-t<CLOCKS_PER_SEC);
- echo_off();read_partid();unlock();load_and_go();
-}static int f=14746;
+close_all(void)
+{if(preferences_file)fclose(preferences_file);close_serialia();}
 static void
 kbd_loop(void)
 {char c;
  do
  {printf(">");scanf("%c",&c);
   switch(c)
-  {case's':synchronize(f);break;
+  {case's':synchronize();break;
    case'j':read_partid();break;case'k':read_version();break;
-   case'f':printf("enter crystal frequency (kHz): ");scanf("%i",&f);
-    printf("frequency assumed %i kHz\n",f);break;
+   case'f':printf("enter crystal frequency (kHz): ");scanf("%i",&(args.freq));
+    printf("frequency assumed %i kHz\n",args.freq);break;
    case'e':erase();break;case'u':unlock();break;
    case'p':prepare();break;case'r':read_mem();break;
    case'a':echo_off();break;case'c':copy_mem();break;
-   case'b':write_file("elk-rom.bin");break;case'l':load_and_go();break;
+   case'b':write_file();break;case'l':load_and_go();break;
    case'h':help();break;
-   case't':no_preferences=!no_preferences;
+   case't':no_preferences=!0;
     printf("%spreferences will be loaded\n",no_preferences?"no ":"");
+    break;
   } 
  }while(c!='q');
 }
+const char*argp_program_version="elk"PACKAGE_VERSION_COMMENTED;
+#define KEY_ASSIGNMENTS_HELP "some key assignments:\n"\
+" j: read part ID\n k: read version\n s: synchronize\n"\
+" l: load and run RAM-based program\n u: unlock uC for writing\n"\
+" p: prepare to erase ROM;  e: erase ROM\n b: load ROM-based program\n"\
+" r: dump MCU memory on screen\n q: quit\n h: commands info\n"\
+" t: toggle preferences loading\n"\
+"For more info, see elk sources and LPC21xx bootstrap loader documentation\n"
+void
+help(void){printf(KEY_ASSIGNMENTS_HELP);}
+const char*argp_program_bug_address ="<"PACKAGE_BUGREPORT">";
+static char doc[]="LPC213x programmer (hardly functional)\v"
+ "On POSIX systems the port baud rate is set according\n"
+ "to the crystal frequency; on Windows it is 115200\n\n"
+ KEY_ASSIGNMENTS_HELP;
+static struct argp_option options[]=
+{{"device",'d',"PORT",0,
+  "Open PORT [/dev/ttyS0 on GNU, COM1 on Windows]"
+ },
+ {"directory",'D',"DIR",0,
+  "Search for programs in DIR [build-time defined]"
+ },
+ {"print-configuration",'c',0,0,
+  "Print build-time configuration and exit"
+ },
+ {"frequency",'f',"FREQ",0,
+  "Assume MCU crystal frequency to be FREQ kHz [14746]"
+ },
+ {"target",'t',"PROGRAM",0,
+  "Name of the program to be loaded [multa]"},
+ {"write",'w',0,0,
+  "Write the program into the flash and quit"
+ },
+ {"run",'r',0,0,"Load the program into RAM, run it and quit"},
+ {"no-prefs",'n',0,0,
+  "Don't load preferences after the program is loaded into RAM"
+ },
+ {"verbose",'v',"LEVEL",OPTION_ARG_OPTIONAL,"Be verbose"},
+ {"verbous",0,"LEVEL",OPTION_ALIAS},
+ {"quiet",'q',0,0,"Be quiet"},
+ {0}
+};
+static error_t
+parse_opt(int key,char*arg,struct argp_state*state)
+{struct arguments*arguments=state->input;int n=0,r;
+ switch(key)
+ {case'c':
+   printf("data prefix: '%s'\n",PACKAGE_DATA_DIR);
+   printf("sources directory: '%s'\n",SOURCE_DIR);
+   printf("preferences file name: '%s'\n",prefs_file_name);
+   exit(normal_exit);
+   break;
+  case'd':arguments->port_name=arg;break;
+  case'D':
+   arguments->directory=arg;arguments->package_data_dir=0;
+   break;
+  case'f':r=sscanf(arg,"%i%n",&(arguments->freq),&n);
+   if(r!=1||arg[n]||arguments->freq<=10)
+   {error("'%s' is not a valid frequency (should be an integer > 10)\n",arg);
+    return ARGP_ERR_UNKNOWN;
+   }break;
+  case'n':no_preferences=!0;break;
+  case'q':arguments->verbosity--;break;
+  case'r':arguments->run=!0;break;
+  case't':arguments->target_name=arg;break; 
+  case'v':if(!arg){arguments->verbosity++;break;}
+   r=sscanf(arg,"%i%n",&(arguments->verbosity),&n);
+   if(r!=1||arg[n])
+   {error("'%s' is not a valid verbosity level"
+     " (should be an integer)\n",arg);
+    return ARGP_ERR_UNKNOWN;
+   }break; 
+  case'w':arguments->write=!0;break;
+  case ARGP_KEY_ARG:if(state->arg_num>=0)argp_usage(state);break;
+  case ARGP_KEY_END:if(state->arg_num<0)argp_usage(state);break;
+  default:return ARGP_ERR_UNKNOWN;
+ }return 0;
+}static struct argp argp={options,parse_opt,0,doc};
 int
 main(int argc,char**argv)
-{char c=!0;int err,freq_arg;
- if(argc>2)
- {sscanf(argv[2],"%i",&freq_arg);if((c=freq_arg>10))f=freq_arg;
-  if(freq_arg<-1)no_preferences=!0;
- }else no_preferences=!0;
- chdir(PACKAGE_DATA_DIR);chdir(package_data_dir);
- usage();printf("crystal frequency assumed %i kHz\n",f);
- drain_uart();
- if((err=init(argc<2?0:argv[1],f)))return err;
- if(!c){program_ram(f);close_all();return 0;}echo_off();read_partid();
- kbd_loop();close_all();return 0;
+{enum program_exit_codes err;
+ args.run=args.write=0;
+ args.freq=14746;args.target_name="multa";
+ args.directory=args.package_data_dir=args.port_name=0;
+ init_error_dir(*argv,SOURCE_DIR);
+ argp_parse(&argp,argc,argv,0,0,&args);
+ if(!args.directory)
+ {args.directory=PACKAGE_DATA_DIR;
+  args.package_data_dir="stribog-target";
+ }
+ printf("crystal frequency assumed %i kHz\n",args.freq);
+ if(!args.target_name&&(args.write||args.run))
+ {error("target not specified\n");return wrong_args;}
+ atexit(close_all);
+ preferences_file=fopen(prefs_file_name,"rb");
+ if(chdir(args.directory))
+ {error("can't chdir to '%s'\n",args.directory);return no_directory;}
+ if(args.package_data_dir&&chdir(args.package_data_dir))
+ {error("can't chdir to '%s'\n",args.package_data_dir);
+  return no_directory;
+ }
+ err=init(args.port_name,args.freq);if(normal_exit!=err)return err;
+ if(args.write||args.run)
+ {clock_t t;synchronize();t=clock();while(clock()-t<CLOCKS_PER_SEC);
+  echo_off();read_partid();unlock();
+ }
+ if(args.write)
+ {prepare_sectors(0,8);erase_sectors(0,8);write_file();}
+ if(args.run)load_and_go();
+ if(args.write||args.run)return normal_exit;
+ echo_off();read_partid();kbd_loop();return normal_exit;
 }
