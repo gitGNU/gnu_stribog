4f73958f4360b4dfa26a3efcbe05c80a99c429ee
P.Kharitonov <ineiev@users.sourceforge.net>
2008-04-05 15:32:19 +0400

    made work decently on OpenBSD

--- sw/host/conloq/serialia.c
+++ sw/host/conloq/serialia.c
@@ -21,17 +21,31 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.*/
 #include<sys/fcntl.h>
 #include<termios.h>
 #include<stdio.h>
+#include<errno.h>
 static struct termios vet;/* previous port settings */
 static const char*dv="/dev/ttyS1";/* default device name */
-static int port=-1;/* file descriptor */
-
+static int port=-1;/* device file descriptor */
+static int
+report_error(const char*report)
+{if(get_verbosity()>=pretty_verbose)printf("%s\n",report);}
 int
 initserialia(const char*tty)
 {struct termios nov;if(tty)dv=tty;
+#ifdef O_NDELAY
+ /*workaround for OpenBSD(3.9): it hangs without O_NDELAY*/
+ port=open(dv,O_RDWR|O_NOCTTY|O_NDELAY);
+ if(port!=-1)
+ {int flags;errno=0;flags=fcntl(port,F_GETFL)&~O_NDELAY;
+  if(errno)
+  {report_error("failed to get flags");close(port);return-1;}
+  if(-1==fcntl(port,F_SETFL,flags))
+  {report_error("failed to set flags");close(port);return-1;}
+ }
+#else
  port=open(dv,O_RDWR|O_NOCTTY);
+#endif
  if(get_verbosity()>=pretty_verbose)printf("UART device \"%s\": ",dv);
- if(port<0)
- {if(get_verbosity()>=pretty_verbose)printf("failed to open\n");return-1;}
+ if(port<0){report_error("failed to open");return-1;}
  tcgetattr(port,&vet);nov=vet;
  cfsetospeed(&nov,B115200);cfsetispeed(&nov,B115200);
  nov.c_cflag|=CLOCAL|CREAD;nov.c_cflag&=~PARENB;
@@ -39,7 +53,7 @@ initserialia(const char*tty)
  nov.c_lflag=nov.c_iflag=0;nov.c_cc[VMIN]=0;nov.c_cc[VTIME]=2;
  tcsetattr(port,TCSANOW,&nov);
  if(get_verbosity()>=pretty_verbose)
- {printf("%s\n",port<0?"fail":"success");}
+  printf("%s\n",port<0?"fail":"success");
  return port<0;
 }void
 closeserialia(void)
