3edc2609c4fa99410b119d7b0a1d9e4a1b350045
P.Kharitonov [GNU/Hurd] <ineiev@users.sourceforge.net>
2008-04-13 14:13:06 +0400

    saving settings and reporting actions cleanup

--- sw/host/conloq/serialia.c
+++ sw/host/conloq/serialia.c
@@ -23,12 +23,22 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.*/
 #include<termios.h>
 #include<stdio.h>
 #include<errno.h>
+#include<string.h>
 static struct termios vet;/* previous port settings */
+static int saved_flags;/* previous fcntl flags*/
 static const char*dv=CONLOQ_PORT;/* default device name */
 static int port=-1;/* device file descriptor */
 static void
-report_error(const char*report)
-{if(get_verbosity()>=pretty_verbose)printf("%s\n",report);}
+close_port(void){close(port);port=-1;}
+static void
+report_error(const char*action)
+{int err=errno;if(get_verbosity()<pretty_verbose)return;
+ printf("%s failed: errno %i\n",action,err);
+ printf("system error message \"%s\"",strerror(err));
+}
+static void
+report_action(const char*action)
+{if(get_verbosity()<pretty_verbose)return;printf("%s\n");}
 int
 initserialia(const char*tty)
 {struct termios nov;if(tty)dv=tty;
@@ -40,32 +50,47 @@ initserialia(const char*tty)
  port=open(dv,O_RDWR|O_NOCTTY|O_NDELAY);
  if(port==-1){report_error("failed to open");return-1;}
  if(O_NDELAY)
- {int flags;errno=0;flags=fcntl(port,F_GETFL,0)&~O_NDELAY;
+ {errno=0;saved_flags=fcntl(port,F_GETFL,0);
   if(errno)
-  {report_error("failed to get flags");close(port);port=-1;return-1;}
-  if(-1==fcntl(port,F_SETFL,flags))
-  {report_error("failed to set flags");close(port);port=-1;return-1;}
+  {report_error("failed to get flags");close_port();return-1;}
+  if(-1==fcntl(port,F_SETFL,saved_flags&~O_NDELAY))
+  {report_error("failed to set flags");close_port();return-1;}
  }
- tcgetattr(port,&vet);nov=vet;
+ report_action("getting port attributes");
+ if(tcgetattr(port,&vet));
+ {report_error("tcgetattr");close_port();return-1;}
+ nov=vet;
  cfsetospeed(&nov,B115200);cfsetispeed(&nov,B115200);
  nov.c_cflag|=CLOCAL|CREAD;nov.c_cflag&=~PARENB;
  nov.c_cflag&=~CSIZE;nov.c_cflag|=CS8;nov.c_oflag&=~OPOST;
+ /*actually, GNU/Hurd (0.3 GNU-Mach 1.3.99/Hurd-0.3 i686-AT386 GNU)
+  ignores c_cc[VTIME] and waits forever.
+  this is not fatal because we expect bytes to arrive from the port
+  often enough (typically, thousands per second)*/
  nov.c_lflag=nov.c_iflag=0;nov.c_cc[VMIN]=0;nov.c_cc[VTIME]=2;
- tcsetattr(port,TCSANOW,&nov);
+ report_action("getting port attributes");
+ if(tcsetattr(port,TCSANOW,&nov))
+ {report_error("tcsetattr");close_port();return-1;}
  if(get_verbosity()>=pretty_verbose)
-  printf("%s\n",port<0?"fail":"success");
+  report_action("%s",port<0?"fail":"success");
  return port<0;
 }void
 closeserialia(void)
-{if(0>port)return;
- if(0)
- {/*GNU/Hurd hangs here. we prefer not to return to old settings*/
-  /*TODO as a workaround: close port, open, apply old settings, close again*/
-  report_error("resetting tty attributes..");
-  if(tcsetattr(port,TCSANOW,&vet))
-   report_error("failed to reset attributes");
- }report_error("closing port..");
- if(close(port))report_error("fail");else report_error("success");
+{if(0>port)return;report_action("closing port");
+ /*GNU/Hurd hangs on tcsetattr of port for the second time.
+  we need to close the file first, then reopen it and set previous attrs*/
+ if(close(port)){report_error("close port");port=-1;return;}
+ report_action("opening port again");
+ port=open(dv,O_RDWR|O_NOCTTY|O_NDELAY);
+ if(-1==port){report_error("open port");return;}
+ report_action("resetting tty attributes");
+ if(tcsetattr(port,TCSANOW,&vet))report_error("tcsetattr");
+ report_action("resetting fcntl flags");
+ if(-1==fcntl(port,F_SETFL,saved_flags))
+  report_error("fcntl(port,F_SETFL,saved_flags)");
+ report_action("closing port the second time");
+ if(close(port))report_error("close");
+ else report_action("port has been successfully closed");
  port=-1;
 }int
 lege(void*p,int n){return read(port,p,n);}
