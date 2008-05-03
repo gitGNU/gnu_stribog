d24bf9fdf09b7dc3ed7cea3bee74ee09d83cfb19
P.Kharitonov <ineiev@users.sourceforge.net>
2008-05-03 17:31:55 +0400

    some functions moved from serialia.c to serial.c
    as serialia.c is conditionally compiled,
    these functions wouldn't be available in
    non-standard environments

--- sw/host/elk/serialia.c
+++ sw/host/elk/serialia.c
@@ -3,7 +3,7 @@ Copyright (C) 2006, 2007, 2008
  Ineiev<ineiev@users.sourceforge.net>, super V 93
 This program is a part of the stribog host software section
 
-This program is free software; you can redistribute it and/or modify
+stribog is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation; either version 3 of the License, or
 (at your option) any later version.
@@ -21,7 +21,6 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.*/
 #include<fcntl.h>
 #include<termios.h>
 #include<stdio.h>
-#include<time.h>
 static struct termios vet;/* saved port settings */
 static char dv[]=ELK_PORT;/* default port name */
 static int portd=-1;/* file descriptor */
@@ -58,21 +57,3 @@ close_serialia(void)
 lege(void*p,int n){return read(portd,p,n);}
 int
 scribe(const void*p,int n){return write(portd,p,n);}
-int
-send_bytes(char*s,int n)
-{while(n>0){int k=scribe(s,n);if(k>0){s+=k;n-=k;}}
- return 0;
-}
-void
-drain_uart(void){char s[0x11];while(0<lege(s,sizeof s));}
-/*NB (24 Oct 2007) it is not good, but the serial port is setup 
- without no timeouts, so the programme is drawing CPU cycles, 
- therefore clock() difference will show something not too far 
- from real time. if we setup the timeouts like in ../conloq, 
- this programme doesn't work under the GNU/Hurd/Mach at all*/
-int 
-wait_for_chars(char*s,int N,int timeout)
-{clock_t t;int i=0,n;char*_=s;t=clock();
- do{n=lege(_,N-i);if(n>0){i+=n;_+=n;}}
- while(i<N&&clock()-t<timeout*CLOCKS_PER_SEC/5);return i;
-}
