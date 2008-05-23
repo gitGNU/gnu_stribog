b4dab7689aec8e879151be3df7f980e69d2b37df
P.Kharitonov <ineiev@users.sourceforge.net>
2008-05-23 11:33:56 +0400

    precision type made int rather than unsigned

--- sw/host/utils/lpfilter.c
+++ sw/host/utils/lpfilter.c
@@ -1,4 +1,6 @@
 /*simple low-pass filter for files
+Copyright (C) 2006, 2007, 2008\
+ Ineiev<ineiev@users.sourceforge.net>
 This program is a part of the stribog host software section
 
 This program is free software; you can redistribute it and/or modify
@@ -12,13 +14,11 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with this program. If not, see <http://www.gnu.org/licenses/>.
-
-Copyright (C) 2006, 2007 Ineiev<ineiev@users.sourceforge.net>*/
+along with this program. If not, see <http://www.gnu.org/licenses/>.*/
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-static int N=200,n,i,j;static unsigned prec=2;static double*s;
+static int N=200,n,i,j;static int prec=2;static double*s;
 static void process_string(const char*u)
 {double x;int j;const char*_=u;while(*_==' ')++_;
  for(j=0;j<n;j++){sscanf(_,"%lf",&x);s[j]+=x;_=strchr(_+1,' ');}
@@ -31,7 +31,7 @@ static void init_n(const char*u)
 }void free_all(void){if(s)free(s);}
 int main(int argc,char**argv)
 {static char str[4913];if(argc>1)sscanf(argv[1],"%i",&N);if(N<1)return 1;
- if(argc>2)sscanf(argv[2],"%u",&prec);
+ if(argc>2)sscanf(argv[2],"%i",&prec);if(prec<0)prec=0;
  fgets(str,sizeof(str),stdin);init_n(str);process_string(str);
  while(1)
  {fgets(str,sizeof(str),stdin);if(feof(stdin))break;process_string(str);}
